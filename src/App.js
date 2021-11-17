4405bb19f09c6de98fba83b8799828876c70341e
jadew33 <77248838+jadew33@users.noreply.github.com>
2021-11-16 21:58:48 -0500

    added in routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import "./App.css"
+import './App.css'
 import Form from './todoform'
+import Page from './random'
+import { Routes, Route, Navigate, Link } from 'react-router-dom'
 
 function App () {
+  // Standard
   return (
     <div>
-      <Form/>
+      <Form />
     </div>
   )
+  
+  // Implementing Router
+  // return (
+  //   <div>
+  //     <Link to="/todo">ToDo</Link>
+  //     <Link to="/page">SomeRandomComponent</Link>
+  //     <Routes>
+  //       <Route path='/todo' element={<Form/>}  />
+  //       <Route path='/page' element={<Page/>}  />
+  //     </Routes>
+  //   </div>
+  // )
 }
 
 export default App
